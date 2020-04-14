88929789020e23263662cde144a35c37a217c162
马成达 <machengda@qutoutiao.net>
2020-04-14 15:22:27 +0800

    add: pug

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,8 @@
-<template>
+<template lang="pug">
+
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        seer2
-      </h1>
-      <h2 class="subtitle">
-        My terrific Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
