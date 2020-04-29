54ea070fb465c71349a457122761971a3b786ef6
马成达 <machengda@qutoutiao.net>
2020-04-29 16:08:57 +0800

    s

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,24 @@
 module.exports = {
-  extends: [
-    "@commitlint/config-conventional"
-  ],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', [
-      'upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert'
-    ]],
-    'type-case': [0],
-    'type-empty': [0],
-    'scope-empty': [0],
-    'scope-case': [0],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
-    'header-max-length': [0, 'always', 72]
-  },
-  "husky": {
-    "hooks": {
-      "commit-msg": "commitlint -e $HUSKY_GIT_PARAMS"
-    }
+    'type-enum': [
+      2,
+      'always', [
+        'add',
+        'update',
+        'delete',
+        'feat',
+        'fix',
+        'merge',
+        'chore',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'revert',
+        'test'
+      ]
+    ],
+    'subject-case': [0, 'never', ['lower-case']]
   }
-};
+}
